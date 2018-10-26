28408ca13909daf4069ad25887887caeee6370fa
LoriKougiou <sdi1400076@di.uoa.gr>
2018-10-26 17:25:18 +0300

    comments added

--- files.c
+++ files.c
@@ -1,10 +1,10 @@
 #include "files.h"
 
-FILE* generate_file(FILE* fp,int* lines,char* filename){
+FILE* generate_file(FILE* fp,int* lines,char* filename){ //dimiourgei ena arxeio "filename" panw sta opoia tha vasistei to relation
 	int i;
-	*lines=rand() % 20000 + 1000;
+	*lines=rand() % 200000 + 1000; //exei metaksi 1000 kai 200000 eggrafwn
 
-	fp=fopen(filename,"w");
+	fp=fopen(filename,"w"); //graffei sto arxeio eggrafes ths morfhs [row_id    value]
 	for(i=1;i<=(*lines);i++){
 		fprintf(fp, "%d",i );
 		fprintf(fp, " ");
@@ -12,11 +12,11 @@ FILE* generate_file(FILE* fp,int* lines,char* filename){
 		fprintf(fp, "\n");
 	}
 	fclose(fp);
-	return fp;
+	return fp; //epistrefei ton filepointer
 
 }
 
-int count_lines(FILE *fp){
+int count_lines(FILE *fp){ //metraei tis eggrafes enos arxeiou
 	int ch=0;
 	int lines=0;
 	while(!feof(fp)){
@@ -29,10 +29,10 @@ int count_lines(FILE *fp){
 	return lines;
 }
 
-int store_file(FILE* fp,char* buff,int buff_size,tuple* rel_t,int lines){
+int store_file(FILE* fp,char* buff,int buff_size,tuple* rel_t,int lines){ //pairnei ena arxeio kai apothikevei tis eggrafes tou ws tuples enos relation
 	char* token;
 	int i=0;
-	while (fgets(buff,buff_size,fp)) {
+	while (fgets(buff,buff_size,fp)) { //pairnei grammi -grammi pou kathe grammi einai mia eggrafh
 		//printf("%s\n",buff);
 		token= strtok(buff," ");
 		if(token==NULL){ printf("Something's wrong.Check the datafile\n"); exit(-1);}
